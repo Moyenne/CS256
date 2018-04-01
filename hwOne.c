e71a4477aa95d1a073793804efb01d6e97018f35
Moyenne <suedoo62@gmail.com>
2018-03-31 20:12:15 -0700

    Create hwOne.c

---
+++ hwOne.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * hwOne.c
+ *
+ *  Created on: Mar 31, 2018
+ *      Author: Moyenne
+ */
+
+ int main(int argc, char **argv)
+ {
+   /**
+    * My first pro-... hey, wait
+    * a minute. Didn't we do this
+    * already?
+    */
+	 printf("Hello World\n");
+   
+	 return 0;
+ }
